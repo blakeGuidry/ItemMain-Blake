9a77011bca00618e60c2d0a53c250887dabbc3b3
Blake Guidry <guidry.bd@gmail.com>
2020-05-17 16:06:45 -0500

    Added CustomEvent dispatch to notify NavBar to increment cart count on click

--- client/src/components/purchase/Delivery.js
+++ client/src/components/purchase/Delivery.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import GetBy from './GetBy';
 
-const Delivery = (props) => {
-  return (
-    <div className='store'>
-      <div className='pickup'>
-        <div className='storeInfo'>
-          <div className='purchaseStore'>
-            <span className='status'>Deliver to </span>
-            <span className='atStore'>The North</span>
-            <div className='editStore'>
-              <span>Edit region</span>
+class Delivery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    this.addToCart = this.addToCart.bind(this);
+  }
+
+  addToCart() {
+    const add = new CustomEvent('addToCart');
+    document.dispatchEvent(add);
+  }
+
+  render() {
+    return (
+      <div className='store'>
+        <div className='pickup'>
+          <div className='storeInfo'>
+            <div className='purchaseStore'>
+              <span className='status'>Deliver to </span>
+              <span className='atStore'>The North</span>
+              <div className='editStore'>
+                <span>Edit region</span>
+              </div>
             </div>
           </div>
+          <div className='pick-it-up'>
+            <button className='pick-it-up-button' onClick={this.addToCart}>Ship it</button>
+          </div>
         </div>
-        <div className='pick-it-up'>
-          <button className='pick-it-up-button'>Ship it</button>
-        </div>
+        <GetBy />
       </div>
-      <GetBy />
-    </div>
-  )
+    )
+  }
 }
 
 export default Delivery;
--- client/src/components/purchase/Store.js
+++ client/src/components/purchase/Store.js
@@ -1,31 +1,46 @@
 import React from 'react';
 
-const Store = (props) => {
-  return (
-    <div className='store'>
-      <div className='pickup'>
-        <div className='storeInfo'>
-          <div className='purchaseStore'>
-            <span className='status'>Pick up tomorrow </span>
-            <span>at </span>
-            <span className='atStore'>Braavos</span>
-            <div className='editStore'>
-              <span>Edit store</span>
+class Store extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    this.addToCart = this.addToCart.bind(this);
+  }
+
+  addToCart() {
+    const add = new CustomEvent('addToCart');
+    document.dispatchEvent(add);
+  }
+
+  render() {
+    return (
+      <div className='store'>
+        <div className='pickup'>
+          <div className='storeInfo'>
+            <div className='purchaseStore'>
+              <span className='status'>Pick up tomorrow </span>
+              <span>at </span>
+              <span className='atStore'>Braavos</span>
+              <div className='editStore'>
+                <span>Edit store</span>
+              </div>
+            </div>
+            <div className='stock'>
+              <span>Only 3 left</span>
             </div>
           </div>
-          <div className='stock'>
-            <span>Only 3 left</span>
+          <div className='pick-it-up'>
+            <button className='pick-it-up-button' onClick={this.addToCart}>Pick it up</button>
           </div>
         </div>
-        <div className='pick-it-up'>
-          <button className='pick-it-up-button'>Pick it up</button>
+        <div className='order-pickup'>
+          <span>Ready tomorrow</span> with Order Pickup
         </div>
       </div>
-      <div className='order-pickup'>
-        <span>Ready tomorrow</span> with Order Pickup
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Store;
